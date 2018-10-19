11ee7c34ed969d4c2b1b21fe3e1c14f5ea3fc36b
deana <deanalmira@gmail.com>
2018-10-19 19:18:13 +0700

    paginator fitur 9

--- src/main/java/com/apap/tugas1/controller/PegawaiController.java
+++ src/main/java/com/apap/tugas1/controller/PegawaiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.apap.tugas1.model.InstansiModel;
 import com.apap.tugas1.model.JabatanModel;
 import com.apap.tugas1.model.PegawaiModel;
 import com.apap.tugas1.service.InstansiService;
@@ -36,6 +37,11 @@ public class PegawaiController {
 		//Fitur 6: Menampilkan Data Suatu Jabatan
 		List<JabatanModel> listJabatan = jabatanService.getAllJabatan();
 		model.addAttribute("listJabatan", listJabatan);
+		
+		//Fitur 10: Menampilkan Pegawai Termuda dan Tertua di Setiap Instansi
+		List<InstansiModel> listInstansi = instansiService.getAllInstansi();
+		model.addAttribute("listInstansi", listInstansi);
+		
 		return "home";
 	}
 	
@@ -49,5 +55,27 @@ public class PegawaiController {
 		model.addAttribute("jabatanPegawai", jabatanPegawai);
 		model.addAttribute("gaji", gaji);
 		return "viewPegawai";
+	}
+	
+	//Fitur 10: Menampilkan Pegawai Termuda dan Tertua di Setiap Instansi
+	@RequestMapping(value = "/pegawai/termuda-tertua", method = RequestMethod.GET)
+	public String viewPegawaiTermudaTertua(@RequestParam (value = "idInstansi") long id, Model model) {
+		
+		InstansiModel instansi = instansiService.getInstansiDetailById(id);
+		
+		PegawaiModel pegawaiTermuda = instansi.getListPegawai().get(instansi.getListPegawai().size()-1);
+		PegawaiModel pegawaiTertua = instansi.getListPegawai().get(0);
+		
+		String jabatanPegawaiTermuda = instansi.getNama();
+		String jabatanPegawaiTertua = instansi.getNama();
+		
+		
+		model.addAttribute("pegawaiTermuda", pegawaiTermuda);
+		model.addAttribute("jabatanPegawaiTermuda", instansi.getNama());
+		
+		model.addAttribute("pegawaiTertua", pegawaiTertua);
+		model.addAttribute("jabatanPegawaiTertua", instansi.getNama());
+		
+		return "view-pegawai-termuda-tertua";
 	}
 }
--- src/main/java/com/apap/tugas1/service/InstansiService.java
+++ src/main/java/com/apap/tugas1/service/InstansiService.java
@@ -1,5 +1,13 @@
 package com.apap.tugas1.service;
 
+import java.util.List;
+
+import com.apap.tugas1.model.InstansiModel;
+
 public interface InstansiService {
+
+	List<InstansiModel> getAllInstansi();
+
+	InstansiModel getInstansiDetailById(long id);
 
 }
